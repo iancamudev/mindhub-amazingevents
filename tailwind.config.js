d3c1d1042b88db8444bd81f220548f767244bad5
Ian Camu <iancamu01@hotmail.com>
2023-04-13 16:35:54 -0300

    Tailwind y un monton de cosas

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat"],
+    },
+    // colors: {
+    //   fucsia: "#D90368",
+    //   grisOscuro: "#1C2829",
+    // },
+    extend: {},
+  },
+  plugins: [],
+};
