860017cad6505b349e4f7a60c052451053a7ce53
Ian Camu <iancamu01@hotmail.com>
2023-04-18 10:11:11 -0300

    Refactor (new functions)

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -1,20 +1,20 @@
 let parseDate = (date) => {
   const arr = date.split("-");
   const months = {
-    1: "Enero",
-    2: "Febrero",
-    3: "Marzo",
-    4: "Abril",
-    5: "Mayo",
-    6: "Junio",
-    7: "Julio",
-    8: "Agosto",
-    9: "Septiembre",
-    10: "Octubre",
-    11: "Noviembre",
-    12: "Diciembre",
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
   };
-  return arr[2] + " de " + months[parseInt(arr[1])] + " de " + arr[0];
+  return months[parseInt(arr[1])] + " " + (arr[2].at(0) == 0 ? arr[2].at(1) : arr[2]) + "th, " + arr[0];
 };
 
 let categoriesGenerator = (categories) => {
@@ -24,4 +24,17 @@ let categoriesGenerator = (categories) => {
 let compare = (event, value) => {
   return (event.name.toLowerCase().includes(value.toLowerCase()) || event.description.toLowerCase().trim().includes(value.toLowerCase()) || 
         event.place.toLowerCase().includes(value.toLowerCase()))
+}
+
+let filterData = ({events, checkList, searchInput, container}) => {
+  let filtrados
+  if (checkList.length) {
+    filtrados = events.filter(
+      (event) => compare(event, searchInput.value) && checkList.includes(event.category));
+  } else {
+    filtrados = events.filter(
+      (event) => compare(event, searchInput.value))
+  } 
+  container.innerHTML = filtrados.map((el) => cardGenerator(el)).join(" ");
+  console.log(filtrados)
 }
--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -1,5 +1,5 @@
 let cardGenerator = (el) => {
-  return `<div
+  return `<a href='../../src/pages/details.html?id=${el._id}'
         class="h-[300px] rounded-2xl flex flex-col justify-between text-white bg-cover bg-center"
         style="background-image: url(${el.image})"
       >
@@ -12,8 +12,29 @@ let cardGenerator = (el) => {
           <p class="">${el.name}</p>
           <p class="text-sm">${parseDate(el.date)}</p>
         </div>
-      </div>`;
+      </a>`;
 };
+
+let detailTemplate = (el) => {
+  return `<div class="w-full sm:w-[80vw] lg:w-[70vw] flex flex-col gap-4">
+  <img src="${el.image}" alt="imagen ilustrativa de evento" class="h-[200px] sm:h-[400px] w-full object-cover" id='image'>
+  <div class="flex flex-col gap-2 text-white mx-4 sm:mx-0">
+      <div class="flex flex-row justify-between">
+          <p class="bg-[#D90368] text-white py-1 px-4 flex flex-row justify-center w-fit rounded-xl">${el.category}</p>
+          <button class="bg-green-600 py-1 px-4 flex flex-row items-baseline w-fit rounded-sm text-white gap-2"><i class="fa-solid fa-plus"></i>Add to cart</button>
+      </div>
+      <div class="flex flex-row gap-4 items-center">
+        <h2 class="text-2xl">${el.name}</h2>
+        <p class="text-[#D90368] text-xl">$${el.price} USD</p>
+      </div>
+      <div class="flex flex-row gap-6">
+        <p><i class="fa-solid fa-location-dot"></i> ${el.place}</p>
+        <p><i class="fa-regular fa-calendar"></i> ${parseDate(el.date)}</p>
+      </div>
+      <p>${el.description}</p>
+  </div
+</div>`
+}
 
 let categoryTemplate = (category) => {
   return `<label>${category}
